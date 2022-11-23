561d4030ebde7b38cabc8a4e2f1916fff4222cf3
SeKa11ch1 <116975509+SeKa11ch1@users.noreply.github.com>
2022-11-23 22:42:08 +0200

    WeatherApp

---
+++ WeatherApp/app/src/main/java/com/nau/weatherapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.nau.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nau.weatherapp.fragments.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
+    }
+}
---
+++ WeatherApp/app/src/main/java/com/nau/weatherapp/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.nau.weatherapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.nau.weatherapp.adapters.WeatherModel
+
+class MainViewModel: ViewModel() {
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
+}
